638d8bc6275ba6d049dc13801a77a37f791d2ab3
Matheus William <matheuswr89@outlook.com>
2021-11-12 11:51:25 -0300

    first commit

---
+++ Consumir-GitHub/index.js
@@ -0,0 +1,81 @@
+const url = "https://api.github.com/users/";
+let json = {};
+let p = document.getElementsByTagName("p")[0];
+let button = document.getElementsByTagName("button")[0];
+let input = document.getElementsByTagName("input")[0];
+
+button.onclick = async function () {
+  await inicia();
+};
+
+input.addEventListener("keyup", async function (e) {
+  if (e.keyCode == 13) await inicia();
+});
+
+async function inicia() {
+  p.innerHTML = `<p>Carregando....</p><img src="loading.gif">`;
+  if (localStorage.getItem(input.value) === null)
+    await pesquisaUser(input.value);
+  else json = JSON.parse(localStorage.getItem(input.value));
+  mostraResultados();
+}
+
+async function pesquisaUser(user) {
+  json = {};
+  let result = await acessaApi(url + user + "/followers");
+  json = { usuario: user, quant_followers: result.length, followers: [] };
+  for (let i = 0; i < result.length; i++) {
+    json.followers.push({
+      nome: await pegaNomeCompleto(result[i].login),
+      login: result[i].login,
+      repositorios: await pegaRepositoriosUser(result[i].login),
+    });
+  }
+  localStorage.setItem(user, JSON.stringify(json));
+}
+
+async function pegaNomeCompleto(user) {
+  let result = await acessaApi(url + user);
+  return result.name === null ? "-" : result.name;
+}
+
+async function pegaRepositoriosUser(user) {
+  let array = [];
+  let result = await acessaApi(url + user + "/repos");
+  for (let i = 0; i < result.length; i++) {
+    array.push({ name: result[i].name, url: result[i].html_url });
+  }
+  return array;
+}
+
+function mostraResultados() {
+  let result = `<p>Usuario: ${json.usuario}</p><p>${json.quant_followers} seguidores:</p>`;
+  let followers = json.followers;
+  for (let i = 0; i < followers.length; i++) {
+    result += `<p class="user" onclick="mudarEstado(${i})"><b>${
+      followers[i].nome === "-" ? "(Sem nome)" : followers[i].nome
+    } - ${followers[i].login}</b></p>
+        <div id="${i}" style="display: none;">`;
+    let repos = followers[i].repositorios;
+    for (let j = 0; j < repos.length; j++) {
+      result += `<a class="repos" href="${repos[j].url}" target="_blank">${repos[j].name}</a><br/>`;
+    }
+    result += `</div>`;
+  }
+  p.innerHTML = result;
+}
+
+async function acessaApi(url) {
+  const response = await fetch(url, {
+    headers: {
+      authorization: "token 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",
+    },
+  });
+  return await response.json();
+}
+
+function mudarEstado(el) {
+  var display = document.getElementById(el).style.display;
+  if (display == "none") document.getElementById(el).style.display = "block";
+  else document.getElementById(el).style.display = "none";
+}
